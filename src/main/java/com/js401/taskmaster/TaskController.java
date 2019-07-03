75914282b51b227cca9e5d0e861469ae7eb3be3a
jshin83 <jen.kyongah.lee@gmail.com>
2019-07-03 11:06:20 -0700

    completed lab

--- src/main/java/com/js401/taskmaster/TaskController.java
+++ src/main/java/com/js401/taskmaster/TaskController.java
@@ -3,33 +3,65 @@ package com.js401.taskmaster;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class TaskController {
     @Autowired
     TaskRepository taskrepo;
 
+
+    /**
+     * Home route
+     * @return String "hello world"
+     */
     @GetMapping({"/"})
     public String helloWorld() {
         return "hello world";
     }
 
+    /**
+     * Returns all Tasks in DB
+     * @return List, all stored Tasks
+     */
     @GetMapping({"/tasks"})
     public ResponseEntity<Iterable<Task>> findAllTasks() {
         return ResponseEntity.ok(this.taskrepo.findAll());
     }
 
-    @PostMapping({"/tasks/{title}/{description}"})
-    public ResponseEntity<Task> saveNewTask(@PathVariable String title, @PathVariable String description) {
-        Task taskToSave = new Task(title, description);
+    /**
+     * Saves a new task
+     * @param title String, task title
+     * @param description, String description of task
+     * @param assignee String, name of assigned person - could be null or empty
+     * @return Task object json, newly saved task
+     */
+    @PostMapping({"/tasks/{title}/{description}/{assignee}"})
+    public ResponseEntity<Task> saveNewTask(@PathVariable String title, @PathVariable String description,
+                                            @PathVariable String assignee) {
+        Task taskToSave = new Task(title, description, assignee);
         Task savedTask = (Task)this.taskrepo.save(taskToSave);
         return ResponseEntity.ok(savedTask);
     }
 
-    @PostMapping({"/tasks/{id}/state"})
+    /**
+     * Returns all tasks assigned to a user
+     * @param name String, user assigned to tasks
+     * @return List, all tasks assigned to the user
+     */
+    @GetMapping("/users/{name}/tasks")
+    public ResponseEntity<Iterable<Task>> findByAssignedTasks(@PathVariable String name) {
+        return ResponseEntity.ok(taskrepo.findByAssignee(name));
+    }
+
+    /**
+     * Advances status of a task ONLY IF THERE IS AN ASSIGNEE
+     * @param id String, Task id
+     * @return updated Task object
+     */
+    @PutMapping({"/tasks/{id}/state"})
     public ResponseEntity<Task> findById(@PathVariable String id) {
         Task taskToUpdate = this.taskrepo.findByid(id);
         if (taskToUpdate != null) {
@@ -38,5 +70,33 @@ public class TaskController {
         }
 
         return ResponseEntity.ok(taskToUpdate);
+    }
+
+    /**
+     * Updates an assignee if the assignee is not being reassigned to the same task
+     * @param id String, Task id
+     * @param assignee String, name to assign to task
+     * @return Task object, updated Task
+     */
+    @PutMapping({"/tasks/{id}/assign/{assignee}"})
+    public ResponseEntity<Task> assignTask(@PathVariable String id, @PathVariable String assignee) {
+        Task taskToUpdate = this.taskrepo.findById(id).get();
+        if (!(assignee.equals("")) && !(assignee.equals(taskToUpdate.getAssignee()))) {
+            taskToUpdate.setAssignee(assignee);
+            taskToUpdate.setStatus("assigned");
+            this.taskrepo.save(taskToUpdate);
+        }
+        return ResponseEntity.ok(taskToUpdate);
+    }
+
+    /**
+     * Deletes a Task
+     * @param id String, Task id
+     * @return String, task was deleted
+     */
+    @DeleteMapping({"tasks/delete/{id}"})
+    public ResponseEntity<String> deleteTask(@PathVariable String id) {
+        this.taskrepo.delete(this.taskrepo.findByid(id));
+        return ResponseEntity.ok("Task was deleted");
     }
 }
--- src/main/java/com/js401/taskmaster/TaskRepository.java
+++ src/main/java/com/js401/taskmaster/TaskRepository.java
@@ -3,9 +3,11 @@ package com.js401.taskmaster;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @EnableScan
 public interface TaskRepository extends CrudRepository<Task, String> {
     Task findByid(String id);
-
+    List<Task> findByAssignee(String name);
     Task findByTitleAndDescription(String title, String description);
 }
